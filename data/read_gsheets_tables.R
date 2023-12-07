2f7b5ef117555a084038dfcb27f8b7e4acff4251
Lars Schöbitz <lars@lse.de>
2023-12-07 21:34:49 +0100

    Update table

--- data/read_gsheets_tables.R
+++ data/read_gsheets_tables.R
@@ -17,23 +17,23 @@ library(dplyr)
 read_sheet("1rECNyFuJID9bjvc6pPHff5Vz-HIuUcZMJlwQ93Rc_-A") |> 
   write_csv(here::here("data/tables/tbl-01-work-packages.csv"))
 
-## tbl-02-wp-activities --------------------------------------------------
+## tbl-02-wp-activities-research-questions.csv ------------------------------
 
 read_sheet("18-6BHa8NR-g2Qlpu9yVSOEFXmsh6mRsAGjvWZMhBN4I") |> 
   write_csv(here::here("data/tables/tbl-02-wp-activities-research-questions.csv"))
-## tbl-03-wp-activities --------------------------------------------------
+## tbl-03-eth-ord-budget-templa-----------------------------------------------
 
 ## copy/paste the final budget table into the DOCX template
 
 read_sheet("1MJkJN739eI9apa0Wfg1cHmIQgSshE1arCHBPWeFJwDk") |> 
   write_csv(here::here("data/tables/tbl-03-eth-ord-budget-template.csv"))
 
-## tbl-04-wp-activities --------------------------------------------------
+## tbl-04-budget-justification -----------------------------------------------
 
 ## copy/paste the final budget table into the DOCX template
 
 read_sheet("1Z7qNNp1DVB6F38eKfvx8FEUXzNi-ZPVfjtAetBJufdI") |> 
-  write_csv(here::here("data/tables/tbl-01-work-packages"))
+  write_csv(here::here("data/tables/tbl-04-budget-justification.csv"))
 
 
 
