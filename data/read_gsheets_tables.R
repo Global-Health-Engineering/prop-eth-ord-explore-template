bba1b198707b160f8a6ecdba307b841a49c976be
Lars Schöbitz <lars@lse.de>
2023-12-07 21:28:23 +0100

    Initial commit

---
+++ data/read_gsheets_tables.R
@@ -0,0 +1,40 @@
+# header ------------------------------------------------------------------
+
+# This script accesses the tables stored as Google Sheets which contain data
+# for the proposal. Google Sheets are edited manually and data is then read
+# from here and stored locally as CSVs.
+
+# library -------------------------------------------------------------------
+
+library(googlesheets4)
+library(readr)
+library(dplyr)
+
+# script ------------------------------------------------------------------
+
+## tbl-01-work-packages --------------------------------------------------
+
+read_sheet("1rECNyFuJID9bjvc6pPHff5Vz-HIuUcZMJlwQ93Rc_-A") |> 
+  write_csv(here::here("data/tables/tbl-01-work-packages.csv"))
+
+## tbl-02-wp-activities --------------------------------------------------
+
+read_sheet("18-6BHa8NR-g2Qlpu9yVSOEFXmsh6mRsAGjvWZMhBN4I") |> 
+  write_csv(here::here("data/tables/tbl-02-wp-activities-research-questions.csv"))
+## tbl-03-wp-activities --------------------------------------------------
+
+## copy/paste the final budget table into the DOCX template
+
+read_sheet("1MJkJN739eI9apa0Wfg1cHmIQgSshE1arCHBPWeFJwDk") |> 
+  write_csv(here::here("data/tables/tbl-03-eth-ord-budget-template.csv"))
+
+## tbl-04-wp-activities --------------------------------------------------
+
+## copy/paste the final budget table into the DOCX template
+
+read_sheet("1Z7qNNp1DVB6F38eKfvx8FEUXzNi-ZPVfjtAetBJufdI") |> 
+  write_csv(here::here("data/tables/tbl-01-work-packages"))
+
+
+
+
